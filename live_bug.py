cd9dc20d3f53d822b94efc31cab4b073ae94831f
kingkonghon@hotmail.com <kingkonghon@hotmail.com>
2017-02-09 18:34:35 +0800

    not complete

--- live_bug.py
+++ live_bug.py
@@ -5,8 +5,24 @@ url = 'http://www.forexfactory.com/calendar.php'
 page = urllib.urlopen(url)
 soup = BeautifulSoup(page)
 
+pre_id_str = ''
+
+tag_name = []
+
 for event_id in soup.find_all('tr'):
-    if event_id.get('data-eventid') is not None:
-        print event_id.get('data-eventid')
-        for child in event_id.descendants:
+    id_str = event_id.get('data-eventid')
+    if id_str == pre_id_str:
+        continue
+    if id_str is not None:
+        pre_id_str = id_str
+        print id_str
+        temp_buffer = {}
+        for child in event_id.children:
             print child
+            try:
+                cell = child.string
+                print child['class'],cell
+                temp_buffer[child['class'][2]] = cell
+            except TypeError, KeyError:
+                continue
+        print temp_buffer
